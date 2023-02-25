082194f3456bc546ac8ab48488064f889fcc26db
Ozeliurs <ozeliurs@gmail.com>
2023-02-25 17:39:22 +0100

    feat: extract certificates

---
+++ scripts/extract_certificates.py
@@ -0,0 +1,31 @@
+import json
+import base64
+from pathlib import Path
+
+import subprocess
+
+
+# Read Traefik ACME JSON
+ca_file = Path(__file__).parent.parent / "docker-compose/traefik/letsencrypt/acme.json"
+acme = json.loads(ca_file.read_text())
+
+# Select certificates from a specific resolver
+certificates = acme["cloudflare"]["Certificates"]
+
+for certificate in certificates:
+    certificate_data = base64.b64decode(certificate["certificate"])
+    key_data = base64.b64decode(certificate["key"])
+
+    domain_folder = ca_file.parent / f"certificates/{certificate['domain']['main']}"
+    domain_folder.mkdir(parents=True, exist_ok=True)
+
+    # Export certificate and key to file
+    with open(domain_folder/"certificate.pem", "wb") as certfile:
+        certfile.write(certificate_data)
+
+    with open(domain_folder/"key.pem", "wb") as keyfile:
+        keyfile.write(key_data)
+
+    # Export certificate and key to .crt and .key files
+    subprocess.run(["openssl", "x509", "-inform", "pem", "-in", str(domain_folder/"certificate.pem"), "-out", str(domain_folder/"certificate.crt")])
+    subprocess.run(["openssl", "rsa", "-inform", "pem", "-in", str(domain_folder/"key.pem"), "-out", str(domain_folder/"key.key")])
